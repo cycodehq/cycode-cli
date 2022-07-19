ae5279b283b6bf4df23c7984583c14d6fce7791b
michal <michal@cycode.com>
2022-07-19 13:47:01 +0300

    print pretty json

--- cli/printers/json_printer.py
+++ cli/printers/json_printer.py
@@ -1,3 +1,4 @@
+import json
 import click
 from typing import List
 from cli.printers.base_printer import BasePrinter
@@ -9,5 +10,6 @@ class JsonPrinter(BasePrinter):
     def print_results(self, context: click.Context, results: List[DocumentDetections]):
         detections = [detection for document_detections in results for detection in document_detections.detections]
         detections_schema = DetectionSchema(many=True)
-        json_result = detections_schema.dumps(detections)
+        detections_dict = detections_schema.dump(detections)
+        json_result = json.dumps(detections_dict, indent=4)
         click.secho(json_result, fg='white')
