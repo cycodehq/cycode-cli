fbd830152253d287869f592f97d787491f83f79b
michal <michal@cycode.com>
2022-07-19 13:53:14 +0300

    remove debug

--- cli/cycode.py
+++ cli/cycode.py
@@ -76,7 +76,7 @@ def code_scan(context: click.Context, scan_type, client_id, secret, show_secret,
     return 1
 
 
-@code_scan.resultcallback()
+@code_scan.result_callback()
 @click.pass_context
 def finalize(context: click.Context, *args, **kwargs):
     if context.obj["soft_fail"]:
