ac2cb708b2f636247d23bf287b2219e874a78f0b
MichalBor <michal@cycode.com>
2022-08-07 13:22:01 +0300

    CM-13345 - update setup.py (#17)
    * added more details to setup.py
    
    * add encoding

--- setup.py
+++ setup.py
@@ -1,12 +1,18 @@
 from setuptools import find_packages, setup
-
+from pathlib import Path
 from cli import __version__
+
+
+def get_long_description():
+    this_directory = Path(__file__).parent
+    return (this_directory / "README.md").read_text(encoding='utf-8')
+
 
 setup(
     name='cycode',
     version=__version__,
     packages=find_packages(),
-    url='https://github.com/cycodehq/cycode_cli',
+    url='https://github.com/cycodehq-public/cycode-cli',
     license='MIT',
     author='Cycode',
     data_files=[('cyclient', ['cyclient/config.yaml']), ('cli', ['cli/config.yaml'])],
@@ -15,8 +21,10 @@ setup(
             'cycode=cli.cycode:main_cli',
         ]},
     include_package_data=True,
-    author_email='maor@cycode.com',
-    description='',
+    author_email='support@cycode.com',
+    description="Perform secrets/iac scans for your sources using Cycode's engine",
+    long_description=get_long_description(),
+    long_description_content_type="text/markdown",
     install_requires=["click",
                       "requests",
                       "pyyaml",
@@ -33,7 +41,6 @@ setup(
     classifiers=[
         "Environment :: Console",
         "Programming Language :: Python",
-        "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
