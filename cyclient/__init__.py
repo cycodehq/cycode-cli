987f3f85f23c867ed2ac018f520d758bd727b4b2
michal <michal@cycode.com>
2022-10-24 15:05:07 +0300

    ok it works

--- cyclient/__init__.py
+++ cyclient/__init__.py
@@ -1,6 +1,14 @@
+from pathlib import Path
 from .config import logger
 
-__version__ = "0.1.6"
+
+def get_version():
+    this_directory = Path(__file__).parent
+    root_project_directory = Path(this_directory).parent
+    return (root_project_directory / "VERSION.txt").read_text(encoding='utf-8')
+
+
+__version__ = get_version()
 
 __all__ = [
     "logger",
