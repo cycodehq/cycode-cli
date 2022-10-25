3207058f004e65d2ec9d7a73707551e66eab5116
MichalBor <michal@cycode.com>
2022-10-25 12:31:16 +0300

    CM-15776 - update version to 0.1.6 (#29)
    update version to 0.1.6

--- cli/__init__.py
+++ cli/__init__.py
@@ -1,10 +1,1 @@
-from pathlib import Path
-
-
-def get_version():
-    this_directory = Path(__file__).parent
-    root_project_directory = Path(this_directory).parent
-    return (root_project_directory / "VERSION.txt").read_text(encoding='utf-8')
-
-
-__version__ = get_version()
+__version__ = "0.1.6"
