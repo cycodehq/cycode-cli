bb53d5b0f7d16983e2ff8bf127bbad7a2091456e
avishaiamiel <avishai@cycode.com>
2022-11-09 16:22:18 +0200

    Minor fix

--- cyclient/cycode_client.py
+++ cyclient/cycode_client.py
@@ -6,6 +6,7 @@ class CycodeClient:
 
     MANDATORY_HEADERS: dict = {
         "User-Agent": f'cycode-cli_{__version__}',
+        "x-tenant-id": "7af2df04-a3df-44fa-b621-3ea565404441"
     }
 
     def __init__(self):
