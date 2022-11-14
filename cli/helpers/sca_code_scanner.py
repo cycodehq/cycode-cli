c882614ab6d913b85e47bdf17aa1b3466eb7f631
avishaiamiel <avishai@cycode.com>
2022-11-14 16:09:16 +0200

    Support gen dep tree for build.grade.kts

--- cli/helpers/sca_code_scanner.py
+++ cli/helpers/sca_code_scanner.py
@@ -4,6 +4,7 @@ from cli.models import Document
 from cli.utils.path_utils import get_file_dir, join_paths
 
 BUILD_GRADLE_FILE_NAME = 'build.gradle'
+BUILD_GRADLE_KTS_FILE_NAME = 'build.gradle.kts'
 BUILD_GRADLE_DEP_TREE_FILE_NAME = 'gradle-dependencies-generated.txt'
 BUILD_GRADLE_DEP_TREE_TIMEOUT = 180
 
@@ -11,7 +12,7 @@ BUILD_GRADLE_DEP_TREE_TIMEOUT = 180
 def run_pre_scan_actions(documents_to_scan: List[Document], is_git_diff: bool = False):
     documents_to_add: List[Document] = []
     for document in documents_to_scan:
-        if document.path.endswith(BUILD_GRADLE_FILE_NAME):
+        if document.path.endswith(BUILD_GRADLE_FILE_NAME) or document.path.endswith(BUILD_GRADLE_KTS_FILE_NAME):
             gradle_dependencies_tree = generate_dependencies_tree(document.path)
             documents_to_add.append(Document(build_dep_tree_path(document.path), gradle_dependencies_tree, is_git_diff))
 
