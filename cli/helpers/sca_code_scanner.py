1191eebd9f98809e1e5e718a133f1be449f72a54
avishaiamiel <avishai@cycode.com>
2022-11-15 21:16:24 +0200

    Avoid failing the flow if gradle dep gen fails

--- cli/helpers/sca_code_scanner.py
+++ cli/helpers/sca_code_scanner.py
@@ -1,7 +1,8 @@
 from cli.utils.shell_executor import shell
-from typing import List
+from typing import List, Optional
 from cli.models import Document
 from cli.utils.path_utils import get_file_dir, join_paths
+from cyclient import logger
 
 BUILD_GRADLE_FILE_NAME = 'build.gradle'
 BUILD_GRADLE_KTS_FILE_NAME = 'build.gradle.kts'
@@ -13,15 +14,21 @@ def run_pre_scan_actions(documents_to_scan: List[Document], is_git_diff: bool = 
     documents_to_add: List[Document] = []
     for document in documents_to_scan:
         if is_gradle_project(document):
-            gradle_dependencies_tree = generate_dependencies_tree(document.path)
-            documents_to_add.append(Document(build_dep_tree_path(document.path), gradle_dependencies_tree, is_git_diff))
+            gradle_dependencies_tree = try_generate_dependencies_tree(document.path)
+            if gradle_dependencies_tree is not None:
+                documents_to_add.append(
+                    Document(build_dep_tree_path(document.path), gradle_dependencies_tree, is_git_diff))
 
     documents_to_scan.extend(documents_to_add)
 
 
-def generate_dependencies_tree(filename: str) -> str:
+def try_generate_dependencies_tree(filename: str) -> Optional[str]:
     command = ['gradle', 'dependencies', '-b', filename, '-q', '--console', 'plain']
-    gradle_dependencies = shell(command, BUILD_GRADLE_DEP_TREE_TIMEOUT)
+    try:
+        gradle_dependencies = shell(command, BUILD_GRADLE_DEP_TREE_TIMEOUT)
+    except Exception as e:
+        logger.debug('Failed to generate gradle dependencies tree. %s', {'exception': str(e)})
+        return None
 
     return gradle_dependencies
 
