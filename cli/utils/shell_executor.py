09ed2f4cc04a792a1ea9a2bb33380174248efe57
avishaiamiel <avishai@cycode.com>
2022-11-16 11:30:21 +0200

    Log shell command error

--- cli/utils/shell_executor.py
+++ cli/utils/shell_executor.py
@@ -1,6 +1,7 @@
 from typing import List, Optional
 import subprocess
 import click
+from cyclient import logger
 
 TIMEOUT = 60
 
@@ -16,7 +17,8 @@ def shell(command: List[str], timeout: int = TIMEOUT) -> Optional[str]:
             timeout=timeout,
         )
         return result.stdout.decode("utf-8").rstrip()
-    except subprocess.CalledProcessError:
+    except subprocess.CalledProcessError as e:
+        logger.debug('Failed to run shell command. %s', {'command': command, 'exception': str(e)})
         pass
     except subprocess.TimeoutExpired:
         raise click.Abort(f'Command {" ".join(map(str, command))} timed out')
