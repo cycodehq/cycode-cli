18961ee37122c8642d4122596ddadd83f3a4c27c
avishaiamiel <93073140+avishaiamiel@users.noreply.github.com>
2022-12-08 15:34:41 +0200

    CM-17670 - Fix Gradle dependencies location in monitor mode (#49)
    * Fix gradle dependencies location in monitor mode
    
    * Minor refactoring
    
    * Minor CR Changes

--- cli/helpers/sca_code_scanner.py
+++ cli/helpers/sca_code_scanner.py
@@ -1,5 +1,6 @@
-from cli.utils.shell_executor import shell
+import click
 from typing import List, Optional
+from cli.utils.shell_executor import shell
 from cli.models import Document
 from cli.utils.path_utils import get_file_dir, join_paths
 from cyclient import logger
@@ -10,11 +11,14 @@ BUILD_GRADLE_DEP_TREE_FILE_NAME = 'gradle-dependencies-generated.txt'
 BUILD_GRADLE_DEP_TREE_TIMEOUT = 180
 
 
-def run_pre_scan_actions(documents_to_scan: List[Document], is_git_diff: bool = False):
+def run_pre_scan_actions(context: click.Context, documents_to_scan: List[Document], is_git_diff: bool = False):
+    is_monitor_action = context.obj.get('monitor')
+    project_path = context.params.get('path')
     documents_to_add: List[Document] = []
     for document in documents_to_scan:
         if is_gradle_project(document):
-            gradle_dependencies_tree = try_generate_dependencies_tree(document.path)
+            gradle_dependencies_tree = try_generate_dependencies_tree(
+                get_manifest_file_path(document, is_monitor_action, project_path))
             if gradle_dependencies_tree is None:
                 logger.warning('Error occurred while trying to generate gradle dependencies tree. %s',
                                {'filename': document.path})
@@ -25,6 +29,10 @@ def run_pre_scan_actions(documents_to_scan: List[Document], is_git_diff: bool = 
                     Document(build_dep_tree_path(document.path), gradle_dependencies_tree, is_git_diff))
 
     documents_to_scan.extend(documents_to_add)
+
+
+def get_manifest_file_path(document, is_monitor_action, project_path):
+    return join_paths(project_path, document.path) if is_monitor_action else document.path
 
 
 def try_generate_dependencies_tree(filename: str) -> Optional[str]:
