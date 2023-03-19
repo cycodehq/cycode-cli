d519b7ad7364dc8e9d48cb966c89acb8a6ab2d1c
Maor Davidzon <maor@cycode.com>
2023-03-19 16:18:39 +0200

    Revert "remove halo import"
    This reverts commit f6e3777a707239555e3d5e59cecd16fb6aea26ca.

--- cli/code_scanner.py
+++ cli/code_scanner.py
@@ -12,6 +12,8 @@ from uuid import uuid4, UUID
 from git import Repo, NULL_TREE, InvalidGitRepositoryError
 from sys import getsizeof
 
+from halo import Halo
+
 from cli.printers import ResultsPrinter
 from cli.models import Document, DocumentDetections, Severity
 from cli.ci_integrations import get_commit_range
