1bda08eaeb4ff2e2b02527efbb4d56808ea3e124
morsa4406 <104304449+morsa4406@users.noreply.github.com>
2023-03-20 12:57:03 +0200

    CM-21288 Cycode CLI - dev mode via sending request to localhost with fixing routing (#90)

--- tests/cyclient/test_scan_client.py
+++ tests/cyclient/test_scan_client.py
@@ -1,8 +1,10 @@
-from cyclient import scan_client
+from cyclient.scan_config.scan_config_creator import DefaultScanConfig
 
 
 def test_get_service_name():
-    assert scan_client.ScanClient.get_service_name('secret') == 'secret'
-    assert scan_client.ScanClient.get_service_name('iac') == 'iac'
-    assert scan_client.ScanClient.get_service_name('sca') == 'scans'
-    assert scan_client.ScanClient.get_service_name('sast') == 'scans'
+    default_scan_config = DefaultScanConfig()
+
+    assert default_scan_config.get_service_name('secret') == 'secret'
+    assert default_scan_config.get_service_name('iac') == 'iac'
+    assert default_scan_config.get_service_name('sca') == 'scans'
+    assert default_scan_config.get_service_name('sast') == 'scans'
