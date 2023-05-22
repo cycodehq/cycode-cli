0cf86daa9e67a48316bf4e8f60bc9e70f5aef1e1
Ilya Siamionau <ilya.siamionau@cycode.com>
2023-05-22 14:52:26 +0200

    CM-23327 - Fix concat_unique_id on Windows (#116)

--- tests/test_zip_file.py
+++ tests/test_zip_file.py
@@ -1,9 +1,22 @@
+import os
+
 from cycode.cli import zip_file
 
 
-def test_concat_unique_id_to_file_starting_with_seperator():
-    assert zip_file.concat_unique_id('/path/to/file', 'unique_id') == 'unique_id/path/to/file'
+def test_concat_unique_id_to_file_with_leading_slash():
+    filename = os.path.join('path', 'to', 'file')  # we should care about slash characters in tests
+    unique_id = 'unique_id'
+
+    expected_path = os.path.join(unique_id, filename)
+
+    filename = os.sep + filename
+    assert zip_file.concat_unique_id(filename, unique_id) == expected_path
 
 
-def test_concat_unique_id_to_file_starting_without_seperator():
-    assert zip_file.concat_unique_id('path/to/file', 'unique_id') == 'unique_id/path/to/file'
+def test_concat_unique_id_to_file_without_leading_slash():
+    filename = os.path.join('path', 'to', 'file')  # we should care about slash characters in tests
+    unique_id = 'unique_id'
+
+    expected_path = os.path.join(unique_id, *filename.split('/'))
+
+    assert zip_file.concat_unique_id(filename, unique_id) == expected_path
