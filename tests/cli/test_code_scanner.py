805b0f6cceb94ff8af2a9f1a0bef95117d3c32a6
Efrat Israel <efrat@cycode.com>
2023-08-06 19:31:18 +0300

    add test for generate document

--- tests/cli/test_code_scanner.py
+++ tests/cli/test_code_scanner.py
@@ -7,8 +7,11 @@ from click import ClickException
 from git import InvalidGitRepositoryError
 from requests import Response
 
-from cycode.cli.code_scanner import _handle_exception, _is_file_relevant_for_sca_scan
+from cycode.cli import consts
+from cycode.cli.code_scanner import _handle_exception, _is_file_relevant_for_sca_scan, _generate_document
 from cycode.cli.exceptions import custom_exceptions
+from cycode.cli.models import Document
+from cycode.cyclient import logger
 
 if TYPE_CHECKING:
     from _pytest.monkeypatch import MonkeyPatch
@@ -77,3 +80,65 @@ def test_is_file_relevant_for_sca_scan() -> None:
     assert _is_file_relevant_for_sca_scan(path) is True
     path = os.path.join('some_package', 'package.lock')
     assert _is_file_relevant_for_sca_scan(path) is True
+
+
+def test_generate_document() -> None:
+    is_git_diff = False
+
+    # scan_type != iac
+    path = 'path/to/nowhere.txt'
+    content = 'nothing important here'
+
+    non_iac_document = Document(path, content, is_git_diff)
+    generated_document = _generate_document(path, consts.SCA_SCAN_TYPE, content, is_git_diff)
+
+    assert non_iac_document.path == generated_document.path
+    assert non_iac_document.content == generated_document.content
+    assert non_iac_document.is_git_diff_format == generated_document.is_git_diff_format
+
+    # scan_type == iac and tfplan_file == False
+    path = 'path/to/nowhere.tf'
+    content = '''provider "aws" {
+        profile = "chili"
+        region = "us-east-1"
+        }
+
+        resource "aws_s3_bucket" "chili-env-var-test" {
+          bucket = "chili-env-var-test"
+        }'''
+
+    iac_document = Document(path, content, is_git_diff)
+    generated_document = _generate_document(path, consts.INFRA_CONFIGURATION_SCAN_TYPE, content, is_git_diff)
+    assert iac_document.path == generated_document.path
+    assert iac_document.content == generated_document.content
+    assert iac_document.is_git_diff_format == generated_document.is_git_diff_format
+
+    # scan_type == iac and tfplan_file == True
+
+    # path = 'path/to/nowhere.json'
+    content = '''
+    {
+       "resource_changes":[
+          {
+             "type":"aws_s3_bucket_public_access_block",
+             "name":"efrat-env-var-test",
+             "change":{
+                "actions":[
+                   "create"
+                ],
+                "after":{
+                   "block_public_acls":false,
+                   "block_public_policy":true,
+                   "ignore_public_acls":false,
+                   "restrict_public_buckets":true
+                }
+             }
+          ]
+       }
+    '''
+
+    generated_tfplan_document = _generate_document(path, consts.INFRA_CONFIGURATION_SCAN_TYPE, content, is_git_diff)
+
+    assert generated_tfplan_document.path.endswith('.tf')
+    assert generated_tfplan_document.content
+    assert generated_tfplan_document.is_git_diff_format == is_git_diff
