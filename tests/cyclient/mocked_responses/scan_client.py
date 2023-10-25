09311844f18f4d3e4eba50b0ee3c00483179009a
Ilya Siamionau <ilya.siamionau@cycode.com>
2023-10-25 13:55:13 +0200

    fix windows

--- tests/cyclient/mocked_responses/scan_client.py
+++ tests/cyclient/mocked_responses/scan_client.py
@@ -126,7 +126,7 @@ def get_scan_detections_url(scan_client: ScanClient) -> str:
 
 
 def get_scan_detections_response(url: str, scan_id: UUID, zip_content_path: Path) -> responses.Response:
-    with open(MOCKED_RESPONSES_PATH.joinpath('detections.json')) as f:
+    with open(MOCKED_RESPONSES_PATH.joinpath('detections.json'), encoding='UTF-8') as f:
         content = f.read()
         content = content.replace('%FILEPATH%', str(zip_content_path.absolute()))
         content = content.replace('%SCAN_ID%', str(scan_id))
