094a260ac9d924a46ba5900f0d88ccd4d5b6dc32
Ilya Siamionau <ilya.siamionau@cycode.com>
2023-12-04 14:32:08 +0100

    mock report status

--- tests/cli/test_main.py
+++ tests/cli/test_main.py
@@ -1,5 +1,6 @@
 import json
 from typing import TYPE_CHECKING
+from uuid import uuid4
 
 import pytest
 import responses
@@ -7,6 +8,7 @@ from click.testing import CliRunner
 
 from cycode.cli.main import main_cli
 from tests.conftest import CLI_ENV_VARS, TEST_FILES_PATH, ZIP_CONTENT_PATH
+from tests.cyclient.mocked_responses.scan_client import mock_scan_responses
 from tests.cyclient.test_scan_client import get_zipped_file_scan_response, get_zipped_file_scan_url
 
 _PATH_TO_SCAN = TEST_FILES_PATH.joinpath('zip_content').absolute()
@@ -27,12 +29,11 @@ def _is_json(plain: str) -> bool:
 @pytest.mark.parametrize('output', ['text', 'json'])
 def test_passing_output_option(output: str, scan_client: 'ScanClient', api_token_response: responses.Response) -> None:
     scan_type = 'secret'
+    scan_id = uuid4()
 
+    mock_scan_responses(responses, scan_type, scan_client, scan_id, ZIP_CONTENT_PATH)
     responses.add(get_zipped_file_scan_response(get_zipped_file_scan_url(scan_type, scan_client), ZIP_CONTENT_PATH))
     responses.add(api_token_response)
-    # Scan report is not mocked.
-    # This raises connection error on the attempt to report scan.
-    # It doesn't perform real request
 
     args = ['--output', output, 'scan', '--soft-fail', 'path', str(_PATH_TO_SCAN)]
     result = CliRunner().invoke(main_cli, args, env=CLI_ENV_VARS)
