7ecd6a5be035e93ba140609cfe1bb67ca9ba3c7d
Ilya Siamionau <ilya.siamionau@cycode.com>
2024-01-15 21:12:28 +0100

    CM-31321 - Fix multiprocessing module in packaged CLI with Python 3.12 on macOS

--- cycode/cli/main.py
+++ cycode/cli/main.py
@@ -1,4 +1,11 @@
+from multiprocessing import freeze_support
+
 from cycode.cli.commands.main_cli import main_cli
 
 if __name__ == '__main__':
+    # DO NOT REMOVE OR MOVE THIS LINE
+    # this is required to support multiprocessing in executables files packaged with PyInstaller
+    # see https://pyinstaller.org/en/latest/common-issues-and-pitfalls.html#multi-processing
+    freeze_support()
+
     main_cli()
