a2587d00853d26aa3aa3a523bf5dcd4b5820a31c
Sara Montefiore <sara@cycode.com>
2024-05-22 09:45:03 +0300

    CM-34882-add tests

--- tests/test_code_scanner.py
+++ tests/test_code_scanner.py
@@ -4,12 +4,20 @@ from uuid import uuid4
 import pytest
 import responses
 
-from cycode.cli.commands.scan.code_scanner import _try_get_report_url_if_needed
+from cycode.cli.commands.scan.code_scanner import (
+    _try_get_aggregation_report_url_if_needed,
+    _try_get_report_url_if_needed,
+)
 from cycode.cli.config import config
 from cycode.cli.files_collector.excluder import _is_relevant_file_to_scan
 from cycode.cyclient.scan_client import ScanClient
 from tests.conftest import TEST_FILES_PATH
-from tests.cyclient.mocked_responses.scan_client import get_scan_report_url, get_scan_report_url_response
+from tests.cyclient.mocked_responses.scan_client import (
+    get_scan_aggregation_report_url,
+    get_scan_aggregation_report_url_response,
+    get_scan_report_url,
+    get_scan_report_url_response,
+)
 
 
 def test_is_relevant_file_to_scan_sca() -> None:
@@ -37,3 +45,39 @@ def test_try_get_report_url_if_needed_return_result(
     scan_report_url_response = scan_client.get_scan_report_url(str(scan_id), scan_type)
     result = _try_get_report_url_if_needed(scan_client, True, str(scan_id), scan_type)
     assert result == scan_report_url_response.report_url
+
+
+@pytest.mark.parametrize('scan_type', config['scans']['supported_scans'])
+def test_try_get_aggregation_report_url_if_no_report_command_needed_return_none(
+    scan_type: str, scan_client: ScanClient
+) -> None:
+    aggregation_id = uuid4().hex
+    scan_parameter = {}
+    result = _try_get_aggregation_report_url_if_needed(scan_parameter, scan_client, scan_type, aggregation_id)
+    assert result is None
+
+
+@pytest.mark.parametrize('scan_type', config['scans']['supported_scans'])
+def test_try_get_aggregation_report_url_if_no_aggregation_id_needed_return_none(
+    scan_type: str, scan_client: ScanClient
+) -> None:
+    scan_parameter = {'report': True}
+    result = _try_get_aggregation_report_url_if_needed(scan_parameter, scan_client, scan_type, '')
+    assert result is None
+
+
+@pytest.mark.parametrize('scan_type', config['scans']['supported_scans'])
+@responses.activate
+def test_try_get_aggregation_report_url_if_needed_return_result(
+    scan_type: str, scan_client: ScanClient, api_token_response: responses.Response
+) -> None:
+    aggregation_id = uuid4()
+    scan_parameter = {'report': True}
+    url = get_scan_aggregation_report_url(aggregation_id, scan_client, scan_type)
+    responses.add(api_token_response)  # mock token based client
+    responses.add(get_scan_aggregation_report_url_response(url, aggregation_id))
+
+    scan_aggregation_report_url_response = scan_client.get_scan_aggregation_report_url(str(aggregation_id), scan_type)
+
+    result = _try_get_aggregation_report_url_if_needed(scan_parameter, scan_client, scan_type, str(aggregation_id))
+    assert result == scan_aggregation_report_url_response.report_url
