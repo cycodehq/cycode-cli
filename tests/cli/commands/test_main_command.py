be7938865399a9d1593123dc79f96091efecf8c0
Ilya Siamionau <ilya.siamionau@cycode.com>
2024-07-18 10:43:47 +0200

    CM-38309 - Cover optional System Git Executable with more tests (#237)

--- tests/cli/commands/test_main_command.py
+++ tests/cli/commands/test_main_command.py
@@ -7,6 +7,7 @@ import responses
 from click.testing import CliRunner
 
 from cycode.cli.commands.main_cli import main_cli
+from cycode.cli.utils.git_proxy import git_proxy
 from tests.conftest import CLI_ENV_VARS, TEST_FILES_PATH, ZIP_CONTENT_PATH
 from tests.cyclient.mocked_responses.scan_client import mock_scan_responses
 from tests.cyclient.test_scan_client import get_zipped_file_scan_response, get_zipped_file_scan_url
@@ -47,3 +48,39 @@ def test_passing_output_option(output: str, scan_client: 'ScanClient', api_token
         assert 'scan_id' in output
     else:
         assert 'Scan ID' in result.output
+
+
+@responses.activate
+def test_optional_git_with_path_scan(scan_client: 'ScanClient', api_token_response: responses.Response) -> None:
+    mock_scan_responses(responses, 'secret', scan_client, uuid4(), ZIP_CONTENT_PATH)
+    responses.add(get_zipped_file_scan_response(get_zipped_file_scan_url('secret', scan_client), ZIP_CONTENT_PATH))
+    responses.add(api_token_response)
+
+    # fake env without Git executable
+    git_proxy._set_dummy_git_proxy()
+
+    args = ['--output', 'json', 'scan', 'path', str(_PATH_TO_SCAN)]
+    result = CliRunner().invoke(main_cli, args, env=CLI_ENV_VARS)
+
+    # do NOT expect error about not found Git executable
+    assert 'GIT_PYTHON_GIT_EXECUTABLE' not in result.output
+
+    # reset the git proxy
+    git_proxy._set_git_proxy()
+
+
+@responses.activate
+def test_required_git_with_path_repository(scan_client: 'ScanClient', api_token_response: responses.Response) -> None:
+    responses.add(api_token_response)
+
+    # fake env without Git executable
+    git_proxy._set_dummy_git_proxy()
+
+    args = ['--output', 'json', 'scan', 'repository', str(_PATH_TO_SCAN)]
+    result = CliRunner().invoke(main_cli, args, env=CLI_ENV_VARS)
+
+    # expect error about not found Git executable
+    assert 'GIT_PYTHON_GIT_EXECUTABLE' in result.output
+
+    # reset the git proxy
+    git_proxy._set_git_proxy()
