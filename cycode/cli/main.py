bb8ced983ce4042180a0d357644339bd075f4413
Ilya Siamionau <ilya.siamionau@cycode.com>
2024-07-18 09:24:18 +0200

    CM-38264 - Fix certification validation

--- cycode/cli/main.py
+++ cycode/cli/main.py
@@ -1,5 +1,9 @@
 from multiprocessing import freeze_support
 
+# DO NOT REMOVE OR MOVE THIS LINE
+# this is required to use certificates system store with requests packaged with PyInstaller
+import pip_system_certs.wrapt_requests  # noqa: F401
+
 from cycode.cli.commands.main_cli import main_cli
 from cycode.cli.sentry import add_breadcrumb, init_sentry
 
