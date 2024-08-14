89bdafd2b75ca12490e04b60220d3330ff8f8cc3
avishaiamiel <avishai@cycode.com>
2024-08-14 21:38:05 +0300

    CM-39300 - Fix UT

--- tests/cli/files_collector/iac/test_tf_content_generator.py
+++ tests/cli/files_collector/iac/test_tf_content_generator.py
@@ -1,4 +1,5 @@
 import os
+import re
 
 from cycode.cli.files_collector.iac import tf_content_generator
 from cycode.cli.utils.path_utils import get_file_content, get_immediate_subdirectories
@@ -13,4 +14,6 @@ def test_generate_tf_content_from_tfplan() -> None:
         tfplan_content = get_file_content(os.path.join(_PATH_TO_EXAMPLES, example, 'tfplan.json'))
         tf_expected_content = get_file_content(os.path.join(_PATH_TO_EXAMPLES, example, 'tf_content.txt'))
         tf_content = tf_content_generator.generate_tf_content_from_tfplan(example, tfplan_content)
-        assert tf_content == tf_expected_content
+
+        cleaned_tf_content = re.sub(r'-[a-fA-F0-9\-]{36}', '', tf_content)
+        assert cleaned_tf_content == tf_expected_content
