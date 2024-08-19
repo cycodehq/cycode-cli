4f92247134e876489cc5c42a42080cde6ab0b125
avishaiamiel <avishai@cycode.com>
2024-08-19 11:30:31 +0300

    CM-3937 - Fix linter

--- tests/cli/files_collector/iac/test_tf_content_generator.py
+++ tests/cli/files_collector/iac/test_tf_content_generator.py
@@ -1,5 +1,4 @@
 import os
-import re
 
 from cycode.cli.files_collector.iac import tf_content_generator
 from cycode.cli.utils.path_utils import get_file_content, get_immediate_subdirectories
@@ -14,5 +13,5 @@ def test_generate_tf_content_from_tfplan() -> None:
         tfplan_content = get_file_content(os.path.join(_PATH_TO_EXAMPLES, example, 'tfplan.json'))
         tf_expected_content = get_file_content(os.path.join(_PATH_TO_EXAMPLES, example, 'tf_content.txt'))
         tf_content = tf_content_generator.generate_tf_content_from_tfplan(example, tfplan_content)
-        
+
         assert tf_content == tf_expected_content
