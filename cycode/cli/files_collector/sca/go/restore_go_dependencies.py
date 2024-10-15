60e57777d94c129f3c83111c1f5d98903337a920
naftali-hershler <naftali@cycode.com>
2024-10-15 14:23:40 +0300

    CM-40909 add go restore

--- cycode/cli/files_collector/sca/go/restore_go_dependencies.py
+++ cycode/cli/files_collector/sca/go/restore_go_dependencies.py
@@ -6,28 +6,21 @@ import click
 from cycode.cli.files_collector.sca.base_restore_dependencies import BaseRestoreDependencies
 from cycode.cli.models import Document
 
-NPM_PROJECT_FILE_EXTENSIONS = ['.json']
-NPM_LOCK_FILE_NAME = 'package-lock.json'
-NPM_MANIFEST_FILE_NAME = 'package.json'
-OUTPUT_FILE_MANUALLY = False
-
+NPM_PROJECT_FILE_EXTENSIONS = ['.mod']
 
 class RestoreGoDependencies(BaseRestoreDependencies):
     def __init__(self, context: click.Context, is_git_diff: bool, command_timeout: int) -> None:
-        super().__init__(context, is_git_diff, command_timeout, OUTPUT_FILE_MANUALLY)
+        super().__init__(context, is_git_diff, command_timeout, True)
 
     def is_project(self, document: Document) -> bool:
         return any(document.path.endswith(ext) for ext in NPM_PROJECT_FILE_EXTENSIONS)
 
     def get_command(self, manifest_file_path: str) -> List[str]:
-        return ['npm', 'install', '--prefix', self.prepare_manifest_file_path_for_command(manifest_file_path),
-                '--package-lock-only', '--ignore-scripts', '--no-audit']
+        return ['go', 'list', '-m', '-json', 'all']
 
     def get_lock_file_name(self) -> str:
-        return NPM_LOCK_FILE_NAME
+        return None
 
     def verify_restore_file_already_exist(self, restore_file_path: str) -> bool:
         return os.path.isfile(restore_file_path)
 
-    def prepare_manifest_file_path_for_command(self, manifest_file_path: str) -> str:
-        return '/' + manifest_file_path.strip('/' + NPM_MANIFEST_FILE_NAME)
