84b298c75d419c978006212aa81d3f647aa26686
naftalicy <naftali@cycode.com>
2024-11-07 13:10:39 +0200

    CM-41217 - Implement SBT restore support for SCA (#259)

---
+++ cycode/cli/files_collector/sca/sbt/restore_sbt_dependencies.py
@@ -0,0 +1,25 @@
+import os
+from typing import List, Optional
+
+from cycode.cli.files_collector.sca.base_restore_dependencies import BaseRestoreDependencies
+from cycode.cli.models import Document
+
+SBT_PROJECT_FILE_EXTENSIONS = ['sbt']
+SBT_LOCK_FILE_NAME = 'build.sbt.lock'
+
+
+class RestoreSbtDependencies(BaseRestoreDependencies):
+    def is_project(self, document: Document) -> bool:
+        return any(document.path.endswith(ext) for ext in SBT_PROJECT_FILE_EXTENSIONS)
+
+    def get_command(self, manifest_file_path: str) -> List[str]:
+        return ['sbt', 'dependencyLockWrite', '--verbose']
+
+    def get_lock_file_name(self) -> str:
+        return SBT_LOCK_FILE_NAME
+
+    def verify_restore_file_already_exist(self, restore_file_path: str) -> bool:
+        return os.path.isfile(restore_file_path)
+
+    def get_working_directory(self, document: Document) -> Optional[str]:
+        return os.path.dirname(document.absolute_path)
