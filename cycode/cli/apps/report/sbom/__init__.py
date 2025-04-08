bce9ad9467b740fbd1b116fd8d66719fc3771c3f
Ilya Siamionau <ilya.siamionau@cycode.com>
2025-04-08 11:07:43 +0200

    use kebab case instead of snake case for command names

--- cycode/cli/apps/report/sbom/__init__.py
+++ cycode/cli/apps/report/sbom/__init__.py
@@ -7,6 +7,9 @@ from cycode.cli.apps.report.sbom.sbom_command import sbom_command
 app = typer.Typer(name='sbom')
 app.callback(short_help='Generate SBOM report for remote repository by url or local directory by path.')(sbom_command)
 app.command(name='path', short_help='Generate SBOM report for provided path in the command.')(path_command)
-app.command(name='repository_url', short_help='Generate SBOM report for provided repository URI in the command.')(
+app.command(name='repository-url', short_help='Generate SBOM report for provided repository URI in the command.')(
     repository_url_command
 )
+
+# backward compatibility
+app.command(hidden=True, name='repository_url')(repository_url_command)
